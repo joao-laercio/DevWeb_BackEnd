17a146fcf262dce8eedcfb0790e13fcb1e6a4e07
joao-laercio <joaolaerciocpm22@gmail.com>
2022-07-11 16:34:58 -0300

    javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+    function somar() {
+        var n1 = prompt("numero 1:");
+        var n2= prompt("numero 2:");
+
+        var soma = parseInt(n1) + parseInt(n2);
+    }
+    function media() {
+            var n1 = prompt("numero 1:");
+            var n2= prompt("numero 2:");
+            var n3= prompt("numero 3:");
+    
+            var soma = (parseFloat(n1) + parseInt(n2) + parseFloat(n3)) / 3;
+        
+     }
