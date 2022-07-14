6db643f4f2e9685811446dd6060ac15e824db319
joao-laercio <joaolaerciocpm22@gmail.com>
2022-07-14 16:50:17 -0300

    projeto_backend

---
+++ Projeto_page/.history/assets/js/javascript_20220714163421.js
@@ -0,0 +1,2 @@
+<div id="liveAlertPlaceholder"></div>
+<bbutton type="button" class="btn btn-primary" id="liveAlertBtn">Show live alert</button>
---
+++ Projeto_page/.history/assets/js/javascript_20220714163423.js
@@ -0,0 +1,2 @@
+<div id="liveAlertPlaceholder"></div>
+<button type="button" class="btn btn-primary" id="liveAlertBtn">Show live alert</button>
